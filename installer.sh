c540192ee5a139e4e6ced93d31736227d6d5429b
Snowy_Fluffy <snowy.lnnk@gmail.com>
2025-03-20 15:28:30 +0300

    update fix

--- installer.sh
+++ installer.sh
@@ -3,6 +3,10 @@ if [ ! -d /tmp/zapret.installer/ ]; then
     cd /tmp || exit 
     git clone https://github.com/Snowy-Fluffy/zapret.installer.git
 fi
+
+if [[ -d /tmp/zapret.installer/ ]]; then
+    cd /tmp/zapret.installer/ && git pull
+fi
 cd /tmp/zapret.installer || exit
 chmod +x zapret-control.sh
 bash /tmp/zapret.installer/zapret-control.sh
